8792f0d44a9307a9ad2e9f60a60bc944b138c0a2
xyyjry <129936912+xyyjry@users.noreply.github.com>
2025-06-10 21:08:24 +0900

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Compile all Java files
-mkdir target\classes
-javac -d target\classes src\main\java\*.java
-
-rem Run the game
-java -cp target\classes src.main.java.com.xingyang.YutLauncher
-
-pause 
